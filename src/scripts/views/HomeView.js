fd4cc3618c8b829b9616cc0ede72b4938cbf8902
John Ludena <johnludena@gmail.com>
2016-08-03 14:17:41 -0500

    home updates

--- src/scripts/views/HomeView.js
+++ src/scripts/views/HomeView.js
@@ -5,10 +5,14 @@ const HomeView = React.createClass ({
 	render: function(){
 		return(
 				<div id="home">
-					<div className="expanded row">
+					<div id="hero" className="expanded row">
 						<div className="medium-12 medium-centered columns">
-							<h1>Turn your Instagram photos into a real ecommerce store</h1>
-							<a className="button large" href="#login">Get started</a>
+							<div className="pitch">
+								<h1>Storegrafi</h1>
+								<h2>Turn your Instagram photos into a real ecommerce store</h2>
+								<a className="button large" href="#login">Get started</a>
+							</div>
+							
 						</div>
 					</div>
 				</div>
